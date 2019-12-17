14c682cc8ffab585d3b7f396078dfbaa29aa6ca2
root <2309906376@qq.com>
2019-12-17 16:07:14 +0800

    add Demo01.java

---
+++ src/cn/kgc/quickhit/Demo01.java
@@ -0,0 +1,13 @@
+package cn.kgc.quickhit;
+
+/**
+ * @ClassName Demo01
+ * @Description TODO
+ * @Author RenYuWen
+ * @Since 2019/12/17 16:05
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
